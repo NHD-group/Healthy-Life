7296244dc502ca13a7ac67d752dc08374832eea0
singuyenbui293 <singuyenbui293@gmail.com>
2016-08-18 18:57:42 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	HealthyLife/uploadVideoViewController.swift

--- HealthyLife/uploadVideoViewController.swift
+++ HealthyLife/uploadVideoViewController.swift
@@ -18,47 +18,55 @@ class uploadVideoViewController: UIViewController, UIImagePickerControllerDelega
     
     @IBOutlet weak var nameVideoTextField: UITextField!
     
+    @IBOutlet weak var libImagesButton: UIButton!
+    
+    @IBOutlet weak var cameraButton: UIButton!
     
     @IBOutlet weak var desTextField: UITextField!
     
     
     @IBOutlet weak var uploadStatusLabel: UILabel!
     
-    
-    @IBAction func uploadAction(sender: AnyObject) {
+    @IBAction func chooseImageAction(sender: AnyObject) {
         let imagePickerController = UIImagePickerController()
         imagePickerController.allowsEditing = true
         imagePickerController.delegate = self
         imagePickerController.mediaTypes = [kUTTypeImage as String , kUTTypeMovie as String]
         
         presentViewController(imagePickerController, animated: true, completion: nil)
-
     }
     
+    var videoUrl = NSURL()
     
-    @IBAction func cancelAction(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let videoUrl = info[UIImagePickerControllerMediaURL] as? NSURL {
             // we selected a video
+            self.videoUrl = videoUrl
+            libImagesButton.hidden = true
+            cameraButton.hidden = true
             
-            let key = DataService.dataService.userRef.child("yourVideo").childByAutoId().key
-            
-            let uploadTask = FIRStorage.storage().reference().child("videos").child(key).putFile(videoUrl, metadata: nil, completion: { (metadata, error) in
-                if error  != nil {
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func uploadAction(sender: AnyObject) {
+        
+        let key = DataService.dataService.userRef.child("yourVideo").childByAutoId().key
+        
+        let uploadTask = FIRStorage.storage().reference().child("videos").child(key).putFile(videoUrl, metadata: nil, completion: { (metadata, error) in
+            if error  != nil {
+                
+                return
+            } else {
+                if let videoUrl = metadata?.downloadURL()?.absoluteString {
                     
-                    return
-                } else {
-                    if let videoUrl = metadata?.downloadURL()?.absoluteString {
-                        
-                        
-                        
-                        let videoInfo: [String: AnyObject] = ["videoUrl": videoUrl, "name": self.nameVideoTextField.text!, "description": self.desTextField.text!]
-                        
-                        DataService.dataService.userRef.child("yourVideo").child(key).setValue(videoInfo)
-                    }
                     
+<<<<<<< HEAD
+=======
                 }
             })
             uploadTask.observeStatus(.Progress, handler: { (snapshot) in
@@ -66,19 +74,31 @@ class uploadVideoViewController: UIViewController, UIImagePickerControllerDelega
                 if (snapshot.progress?.completedUnitCount) != nil{
                     self.uploadStatusLabel.text = "uploading"
                     //                    String(completedUnitCount)
+>>>>>>> origin/master
                     
+                    let videoInfo: [String: AnyObject] = ["videoUrl": videoUrl, "name": self.nameVideoTextField.text!, "description": self.desTextField.text!]
+                    
+                    DataService.dataService.userRef.child("yourVideo").child(key).setValue(videoInfo)
                 }
                 
-            })
+            }
+        })
+        uploadTask.observeStatus(.Progress, handler: { (snapshot) in
+            print(snapshot)
+            if let completedUnitCount = snapshot.progress?.completedUnitCount{
+                self.uploadStatusLabel.text = "uploading"
+                //                    String(completedUnitCount)
+                
+            }
             
-            uploadTask.observeStatus(.Success, handler: { (snapshot) in
-                self.uploadStatusLabel.text = "done"
-            })
-        }
+        })
         
-        dismissViewControllerAnimated(true, completion: nil)
-        
+        uploadTask.observeStatus(.Success, handler: { (snapshot) in
+            self.uploadStatusLabel.text = "done"
+        })
+
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
